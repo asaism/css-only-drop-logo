5ee228d31bacb20ed1af7882ea7670e419ab23fe
Suguru ASAI <suguru@asaism.com>
2022-01-01 18:41:25 +0900

    Change files name for typescript-ize

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import HelloWorld from './HelloWorld';
+import * as React from "react";
+import ReactDom from "react-dom";
+import HelloWorld from "./HelloWorld";
 
 ReactDom.render(
   <React.StrictMode>
