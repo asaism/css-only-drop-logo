809b4bb492df5d6085792ed0c4b7536efdd112fd
Suguru ASAI <suguru@asaism.com>
2022-01-02 20:42:55 +0900

    Modified css only drop logo

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
 import ReactDom from "react-dom";
 import styles from './styles.css';
+import { LogoApple } from 'react-ionicons';
 
 ReactDom.render(
   <React.StrictMode>
-    <div className={styles.drop}></div>
+    <div className={styles.drop}>
+      <LogoApple
+        color={'#445'}
+        width={'6em'}
+        height={'6em'}
+        shake={true}
+        beat={false}
+        rotate={false}
+      />
+    </div>
   </React.StrictMode>,
   document.getElementById('app')
 );
